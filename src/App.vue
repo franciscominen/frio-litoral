4bdd990329ccde9d0e4fe8482dd358705289ff63
Francisco Minen <fran.minen@gmail.com>
2022-02-15 19:28:03 -0300

    Sass intalled.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-
+    <Home />
   </div>
 </template>
 
 <script>
-
+import Home from "./views/Home.vue";
 export default {
-  name: 'App',
-  components: {
-    
-  }
-}
+  name: "App",
+  components: { Home },
+};
 </script>
 
-<style>
-
+<style lang='scss'>
+@import "./scss/main.scss";
+@import "./scss/variables.scss";
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <main class="home_container">
+    <h1>
+      Encontrá el repuesto<br />
+      que estás buscando<span>.</span>
+    </h1>
+    <ul>
+      <li>
+        <img src="@/assets/img/lavarropa.png" alt="" />
+        <h2>Lavarropas</h2>
+      </li>
+      <li>
+        <img src="@/assets/img/aire.png" alt="" />
+        <h2>
+          Aires<br />
+          Acondicionados
+        </h2>
+      </li>
+      <li>
+        <img src="@/assets/img/heladera.png" alt="" />
+        <h2>Heladeras</h2>
+      </li>
+      <li>
+        <img src="@/assets/img/automotor.png" alt="" />
+        <h2>Automotor</h2>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style lang='scss' src='@/scss/views/home.scss'>
+</style>
